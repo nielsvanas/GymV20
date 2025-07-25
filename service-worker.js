b09e91c4c7d4e5afd0ccfdace363cd1d1e043df6
nielsvanas <76221439+nielsvanas@users.noreply.github.com>
2025-07-25 18:06:04 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "gymv20-cache-v1";
+const const CACHE_NAME = "gymv20a-cache-v1";
 const FILES=["/","index.html","entries.html","body.html","main.js","entries.js","body.js","manifest.json","baki.png","baki_icon.png","https://fonts.googleapis.com/css2?family=Metal+Mania&display=swap","prEntry.html","prLog.html","prEntry.js","prLog.js"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES)));self.skipWaiting();});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME&&caches.delete(k)))));self.clients.claim();});
